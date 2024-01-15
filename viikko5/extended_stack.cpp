69d5bf8d6eeaffe3a1ea88fa245a6590b86600ae
KippaK <antti.venetjoki@gmail.com>
2024-01-15 21:15:33 +0200

    Extended_Stack is now derived from Stack

--- viikko5/extended_stack.cpp
+++ viikko5/extended_stack.cpp
@@ -1,70 +1,16 @@
 #include "stack.h"
 
 template <typename T>
-Error_code Extended_Stack<T>::push(T item) {
-   Error_code outcome = success;
-   if (count >= stack_size)
-      outcome = overflow;
-   else
-      entry[count++] = item;
-   return outcome;
-}
-
-template <typename T>
-Error_code Extended_Stack<T>::pop() {
-   Error_code outcome = success;
-   if (count == 0)
-      outcome = underflow;
-   else
-      --count;
-   return outcome;
-}
-
-template <typename T>
-Error_code Extended_Stack<T>::top(T &item) const {
-   Error_code outcome = success;
-   if (count == 0)
-      outcome = underflow;
-   else
-      item = entry[count - 1];
-   return outcome;
-}
-
-template <typename T>
-bool Extended_Stack<T>::empty() const {
-   return (count == 0);
-}
-
-template <typename T>
 bool Extended_Stack<T>::full() const {
-   return (count == stack_size);
+   return (Stack<T>::count == Stack<T>::stack_size);
 }
 
 template <typename T>
 void Extended_Stack<T>::clear() {
-   count = 0;
+   Stack<T>::count = 0;
 }
 
 template <typename T>
 int Extended_Stack<T>::size() const {
-   return count;
-}
-
-template <typename T>
-Extended_Stack<T>::Extended_Stack() {
-   count = 0;
-   stack_size = DEFAULT_STACK_SIZE;
-   entry = new T[stack_size];
-}
-
-template <typename T>
-Extended_Stack<T>::Extended_Stack(size_t aStack_size) {
-   count = 0;
-   stack_size = aStack_size;
-   entry = new T[stack_size];
-}
-
-template <typename T>
-Extended_Stack<T>::~Extended_Stack() {
-   delete entry;
+   return Stack<T>::count;
 }
--- viikko5/stack.h
+++ viikko5/stack.h
@@ -14,28 +14,16 @@ public:
    Error_code top(T &item) const;
    Error_code push(const T &item);
 
-private:
+protected:
    int count;
    size_t stack_size;
    T *entry = NULL;
 };
 
 template <typename T>
-class Extended_Stack {
+class Extended_Stack : public Stack<T> {
 public:
-   Extended_Stack();
-   Extended_Stack(size_t sStack_size);
-   ~Extended_Stack();
-   Error_code pop();
-   Error_code push(T item);
-   Error_code top(T &item) const;
-   bool empty() const;
    void clear(); // Reset the stack to be empty.
    bool full() const; // If the stack is full, return true; else return false.
    int size() const; // Return the number of entries in the stack.
-
-private:
-   int count;
-   size_t stack_size;
-   T *entry = NULL;
 };

5eaee2d40bd49b6d272ebd3fd673a8a5e35f8ff5
KippaK <antti.venetjoki@gmail.com>
2024-01-15 14:45:20 +0200

    BIG WIP

---
+++ viikko5/tehtava1.cpp
@@ -0,0 +1,15 @@
+#include "stack.h"
+
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+const size_t STACK_SIZ = 256;
+
+int main()
+{
+   Stack<char> s(STACK_SIZE);
+   
+}

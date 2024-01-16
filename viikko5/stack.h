c95c23974eab218acdff24f8872a74fced47b947
KippaK <antti.venetjoki@gmail.com>
2024-01-16 12:48:42 +0200

    moved implementation to header file

--- viikko5/stack.h
+++ viikko5/stack.h
@@ -1,3 +1,5 @@
+// stack.h
+
 #pragma once
 #include "../utility/utility.h"
 
@@ -23,7 +25,108 @@ protected:
 template <typename T>
 class Extended_Stack : public Stack<T> {
 public:
+   Extended_Stack();
+   Extended_Stack(size_t aStack_size);
+   ~Extended_Stack();
    void clear(); // Reset the stack to be empty.
    bool full() const; // If the stack is full, return true; else return false.
    int size() const; // Return the number of entries in the stack.
 };
+
+// stack.cpp
+
+#include "stack.h"
+
+template <typename T>
+Error_code Stack<T>::push(const T &item) {
+   Error_code outcome = success;
+   if (count >= stack_size)
+      outcome = overflow;
+   else
+      entry[count++] = item;
+   return outcome;
+}
+
+template <typename T>
+Error_code Stack<T>::pop() {
+   Error_code outcome = success;
+   if (count == 0)
+      outcome = underflow;
+   else
+      --count;
+   return outcome;
+}
+
+template <typename T>
+Error_code Stack<T>::top(T &item) const {
+   Error_code outcome = success;
+   if (count == 0)
+      outcome = underflow;
+   else
+      item = entry[count - 1];
+   return outcome;
+}
+
+template <typename T>
+bool Stack<T>::empty() const {
+   return (count == 0);
+}
+
+template <typename T>
+Stack<T>::Stack() {
+   count = 0;
+   stack_size = DEFAULT_STACK_SIZE;
+   entry = new T[stack_size];
+}
+
+template <typename T>
+Stack<T>::Stack(size_t aStack_size)
+{
+   count = 0;
+   stack_size = aStack_size;
+   entry = new T[stack_size];
+}
+
+template <typename T>
+Stack<T>::~Stack()
+{
+   delete[] entry;
+}
+// extended_stack.cpp
+
+#include "stack.h"
+
+template <typename T>
+bool Extended_Stack<T>::full() const {
+   return (Stack<T>::count == Stack<T>::stack_size);
+}
+
+template <typename T>
+void Extended_Stack<T>::clear() {
+   Stack<T>::count = 0;
+}
+
+template <typename T>
+int Extended_Stack<T>::size() const {
+   return Stack<T>::count;
+}
+
+template <typename T>
+Extended_Stack<T>::Extended_Stack() {
+   Stack<T>::count = 0;
+   Stack<T>::stack_size = DEFAULT_STACK_SIZE;
+   Stack<T>::entry = new T[Stack<T>::stack_size];
+}
+
+template <typename T>
+Extended_Stack<T>::Extended_Stack(size_t aStack_size) {
+   Stack<T>::count = 0;
+   Stack<T>::stack_size = aStack_size;
+   Stack<T>::entry = new T[Stack<T>::stack_size];
+}
+
+template <typename T>
+Extended_Stack<T>::~Extended_Stack() {
+   delete[] Stack<T>::entry;
+}
+
--- viikko5/tehtava1.cpp
+++ viikko5/tehtava1.cpp
@@ -1,3 +1,5 @@
+// tehtava.cpp
+
 #include "stack.h"
 
 #include <iostream>
@@ -6,10 +8,44 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-const size_t STACK_SIZ = 256;
+const size_t STACK_SIZE = 256;
+
+
+Error_code item_stack_to_stack(
+   Extended_Stack<char> &old_s, 
+   Extended_Stack<char> &new_s)
+{
+   Error_code err = success;
+   char ch;
+   err = old_s.top(ch);
+   err = new_s.push(ch);
+   err = old_s.pop();
+   return err;
+}
+
+Error_code reverse_stack(Extended_Stack<char> &stack) { 
+   Error_code err = success;
+   Extended_Stack<char> new_stack(stack.size());
+   while (!stack.empty()) {
+      err = item_stack_to_stack(stack, new_stack);
+   }
+   stack = new_stack;
+   return err;
+}
+
+Error_code user_input_to_stack(Extended_Stack<char> &stack) 
+{
+   char c;
+   Error_code err = success;
+   while(cin.get(c)) {
+      err = stack.push(c);
+   }
+   return err;
+}
 
 int main()
 {
-   Stack<char> s(STACK_SIZE);
-   
+   Error_code err = success;
+   Extended_Stack<char> s(STACK_SIZE);
+   err = reverse_stack(s);
 }

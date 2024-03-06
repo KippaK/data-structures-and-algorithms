cb020aaf1399e87058cd98ee5bfc58de08ff9ac4
KippaK <antti.venetjoki@gmail.com>
2024-03-06 11:14:05 +0200

    upload

---
+++ viikko11/ratkaisu_kierros_8_tehtava_3.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+using namespace chrono;
+
+
+void move(int count, int start, int finish, int temp)
+{
+    if (count > 0) {
+        move(count - 1, start, temp, finish);
+        // Commented out the user message for timing analysis
+        // cout << "Move disk " << count << " from " << start << " to " << finish << "." << endl;
+        move(count - 1, temp, finish, start);
+    }
+}
+
+void moveIterative(int count, int start, int finish, int temp) {
+    int swap;
+
+    while (count > 0) {
+        move(count - 1, start, temp, finish);
+        // Comment out the line below for analysis
+        cout << "Move disk " << count << " from " << start << " to " << finish << "." << endl;
+        count--;
+        swap = start;
+        start = temp;
+        temp = swap;
+    }
+}
+
+int main()
+{
+    const int maxDisks = 15;  // Set the maximum number of disks for testing
+    high_resolution_clock::time_point t1, t2;
+
+    for (int disks = 10; disks <= maxDisks; ++disks) {
+        t1 = high_resolution_clock::now();  // Record start time
+        move(disks, 1, 3, 2);
+        t2 = high_resolution_clock::now();  // Record end time
+
+        auto duration = duration_cast<milliseconds>(t2 - t1).count();
+        cout << "Number of Disks: " << disks << ", Execution Time: " << duration << " ms" << endl;
+    }
+
+    return 0;
+}
+
